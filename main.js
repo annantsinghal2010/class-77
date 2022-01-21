1387aa400d7c9345d5de238079554a48ff8b6183
annantsinghal2010 <77838184+annantsinghal2010@users.noreply.github.com>
2022-01-21 18:06:53 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,17 @@
+var name1 = "annant";
+var name2 = " singhal";
+var name3 = name1.concat(name2);
+console.log(name1);
+console.log(name2);
+console.log(name3);
+
+var a = 11;
+var b = 10;
+var c = a + b;
+var d =a - b;
+console.log(a);
+console.log(b);
+console.log(c);
+console.log(d);
+console.log(a+" + "+b+" = "+c);
+console.log(a+" - "+b+" = "+d);
